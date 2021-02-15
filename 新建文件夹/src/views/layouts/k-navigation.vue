9ce41f30d7c38048f0b6e208645b0d317a3ed41e
kohai <916309979@qq.com>
2021-02-15 21:56:41 +0800

    init

---
+++ 新建文件夹/src/views/layouts/k-navigation.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="k-navigation flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        lorem
+    </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'KNavigation',
+});
+</script>
